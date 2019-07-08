36cced55407e487fc7b52f90c773fc4a184e601d
SilentResonance <38491750+SilentResonance@users.noreply.github.com>
2019-07-08 19:57:11 +0200

    Added german translation. No clue why the GUI is still shown in English

---
+++ gui/ppm_de.ts
@@ -0,0 +1,183 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de" sourcelanguage="en">
+<context>
+    <name>Loader</name>
+    <message>
+        <location filename="loader.cpp" line="76"/>
+        <source>Reseting the Arduino</source>
+        <translation>Zurücksetzen des Arduino</translation>
+    </message>
+    <message>
+        <location filename="loader.cpp" line="84"/>
+        <source>Retriving the bootloader parameters</source>
+        <translation>Laden der Bootloader Parameter</translation>
+    </message>
+    <message>
+        <location filename="loader.cpp" line="144"/>
+        <source>Uploading firmware</source>
+        <translation>Firmware wird hochgeladen</translation>
+    </message>
+    <message>
+        <location filename="loader.cpp" line="149"/>
+        <source>Validating firmware</source>
+        <translation>Firmware wird validiert</translation>
+    </message>
+    <message>
+        <location filename="loader.cpp" line="260"/>
+        <source>Wait device ready</source>
+        <translation>Warte auf Gerät bereit...</translation>
+    </message>
+    <message>
+        <location filename="loader.cpp" line="274"/>
+        <source>Error: Timeout</source>
+        <translation>Fehler: Timeout</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.cpp" line="48"/>
+        <source>Firmware uploading failed.</source>
+        <translation>Hochladen der Firmware fehlgeschlagen</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="49"/>
+        <source>Please check if the arduino connecterd and right serial port selected.</source>
+        <translation>Bitte prüfen Sie, ob der Arduino verbunden und der richtige Port ausgewählt ist.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="66"/>
+        <source>Stop</source>
+        <translation>Beenden</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="71"/>
+        <location filename="mainwindow.cpp" line="360"/>
+        <source>Start</source>
+        <translation>Start</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="84"/>
+        <source>The device does not respond.</source>
+        <translation>Das Gerät antwortet nicht.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="85"/>
+        <source>Try to update the firmware?</source>
+        <translation>Firmware aktualisieren?</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="111"/>
+        <source>Disconnect</source>
+        <translation>Trennen</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="117"/>
+        <location filename="mainwindow.cpp" line="358"/>
+        <source>Connect</source>
+        <translation>Verbinden</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="290"/>
+        <source>Channel #%1, %:</source>
+        <translation>Kanal Nr.%1 %:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="305"/>
+        <source>Bind</source>
+        <translation>Bind</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="349"/>
+        <source>Arduino PPM Generator</source>
+        <translation>Arduino PPM Generator</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="350"/>
+        <source>Channels count:</source>
+        <translation>Anzahl Kanäle:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="351"/>
+        <source>Period, ms:</source>
+        <translation>Periode, ms:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="352"/>
+        <source>Pause, ms:</source>
+        <translation>Pause, ms:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="353"/>
+        <source>Minimum, ms:</source>
+        <translation>Minimum, ms:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="354"/>
+        <source>Maximum, ms:</source>
+        <translation>Maximum, ms:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="355"/>
+        <source>Serial port:</source>
+        <translation>Serieller Port:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="356"/>
+        <source>Update</source>
+        <translation>Update</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="357"/>
+        <source>Baud rate, Bd:</source>
+        <translation>Baudrate, Bd:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="359"/>
+        <source>Sync period, ms:</source>
+        <oldsource>Sync period, ms</oldsource>
+        <translation>Sync Periode, ms:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="361"/>
+        <source>Inversion</source>
+        <translation>Invertieren</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="362"/>
+        <source>Time, ms</source>
+        <translation>Zeit, ms</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="mainwindow.cpp" line="371"/>
+        <source>Port: %1
+Location: %2
+Description: %3
+Manufacturer: %4
+Vendor Identifier: %5
+Product Identifier: %6
+Busy: %7</source>
+        <translation>Port: %1
+Ort: %2
+Beschreibung: %3
+Hersteller: %4
+Marke: %5
+Produkt: %6
+Beschäftigt: %7</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="386"/>
+        <source>Yes</source>
+        <translation>Ja</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="386"/>
+        <source>No</source>
+        <translation>Nein</translation>
+    </message>
+</context>
+</TS>
